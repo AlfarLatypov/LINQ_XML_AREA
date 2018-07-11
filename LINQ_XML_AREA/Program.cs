4c13501045d7e0be3e84be4ddcdee98d5b76dd0b
ЛатыповА <@237_1>
2018-07-11 20:40:00 +0600

    Добавьте файлы проекта.

---
+++ LINQ_XML_AREA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_XML_AREA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
